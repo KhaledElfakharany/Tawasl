679784675d70bdc95e142d18fa3cd78911165d2d
KhaledElfakharany <K.fakharany@gmail.com>
2017-04-27 14:10:05 +0200

    Initial Commit

---
+++ Tawasl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tawasl
+//
+//  Created by Khaled Elfakharany on 4/27/17.
+//  Copyright © 2017 Khaled Elfakharany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
